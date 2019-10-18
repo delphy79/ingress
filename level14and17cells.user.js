fe10857f89c90f4098c2a1ccecd4eb7d3231a2e5
delphy79 <56617064+delphy79@users.noreply.github.com>
2019-10-18 15:00:28 +0900

    Update level14and17cells.user.js

--- level14and17cells.user.js
+++ level14and17cells.user.js
@@ -652,7 +652,7 @@ function wrapper(plugin_info) {
         color = 'DeepPink';
     }
     if (cell.level == 17) {
-        color = '#B24BE5';
+        color = '#FF50CF';
     }
     if (cell.level == 14) {
         color = 'Red';
