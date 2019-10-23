e346fd6901d437007c7b61f1928dce12c106dea3
delphy79 <56617064+delphy79@users.noreply.github.com>
2019-10-23 14:20:08 +0900

    Update niantic-wayfarer-review-script.user.js

--- niantic-wayfarer-review-script.user.js
+++ niantic-wayfarer-review-script.user.js
@@ -6,8 +6,8 @@
 // @author       DELPHY79
 // @match        https://wayfarer.nianticlabs.com/review
 // @grant        none
-// @downloadURL  https://github.com/delphy79/ingress/raw/master/niantic-wayfarer-review-script.user.js
-// @updateURL    https://github.com/delphy79/ingress/raw/master/niantic-wayfarer-review-script.user.js
+// @downloadURL  https://github.com/delphy79/ingress/raw/master/recon-script.user.js
+// @updateURL    https://github.com/delphy79/ingress/raw/master/recon-script.user.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // ==/UserScript==
 
@@ -20,6 +20,50 @@
 
 async function fn_init() {
     var w = typeof unsafeWindow === 'undefined' ? window : unsafeWindow;
+
+    var css = '.test-red { border: 1px red dashed; }',
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+    head.appendChild(style);
+    style.type = 'text/css';
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+
+    w.onkeydown = function(event) {
+        if (event.keyCode >= 49 && event.keyCode <= 53) {
+            var five_stars = $(".five-stars");
+            var five_stars_obj = null;
+            var check_num = -1;
+            for (var i=0; i<five_stars.length; i++) {
+                if ($(five_stars.get(i)).hasClass("test-red")) {
+                    five_stars_obj = five_stars.get(i);
+                    check_num = i;
+                    break;
+                }
+            }
+            if (five_stars_obj != null) {
+                var five_stars_obj_children = $(five_stars_obj).children("button");
+                $(five_stars_obj_children.get(event.keyCode-49)).click();
+                $(five_stars_obj).removeClass("test-red");
+                if (check_num < 5) {
+                    $(five_stars.get(check_num+1)).addClass("test-red");
+                }
+                if (check_num == 3) {
+                    $("#content-container").animate({scrollTop: $("body").height()}, 500);
+                }
+            }
+        } else if (event.keyCode == 13) {
+            if ($(".modal-dialog").length == 0) {
+                $("#submitDiv").children().click(); //제출
+            } else {
+                $(".modal-dialog").children().children().children().children("button").click(); //다음후보
+            }
+        }
+    }
+
     w.$scope = element => w.angular.element(element).scope();
     var NewSubmissionController = w.document.getElementById('NewSubmissionController');
     var subCtrl = w.$scope(NewSubmissionController).subCtrl;
@@ -34,6 +78,10 @@ async function fn_init() {
                     }
                 }
             });
+            var five_stars = $(".five-stars");
+            if (five_stars.length > 0) {
+                $(five_stars.get(0)).addClass("test-red");
+            }
         }
     }, 100);
 }
