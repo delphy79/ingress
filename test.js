b9958f410858834e60242e9679cfd7fdebcb2612
delphy79 <56617064+delphy79@users.noreply.github.com>
2019-10-31 10:14:59 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
-(function() {
-    'use strict';
-
-    // Your code here...
-    setTimeout(function() {fn_init();}, 250);
-})();
+setTimeout(function() {fn_init();}, 250);
 
 async function fn_init() {
     alert('hello');
