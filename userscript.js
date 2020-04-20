6c434e1b9c911469865e92b95300de91322380a5
delphy79 <56617064+delphy79@users.noreply.github.com>
2020-04-20 11:18:51 +0900

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -3,23 +3,44 @@ setTimeout(function() {fn_init();}, 100);
 function fn_init() {
     var w = typeof unsafeWindow === 'undefined' ? window : unsafeWindow;
     w.$scope = element => w.angular.element(element).scope();
-	
-	var timeElem;
 
-	function createTimer(){
-		var header = document.getElementsByClassName("niantic-wayfarer-logo")[0];
-		var headerTimer = document.createElement("div");
-		headerTimer.innerText = "Time: ";
-		headerTimer.setAttribute("style", "display: inline-block; margin-left: 5em;");
-		timeElem = document.createElement("div");
-		timeElem.innerText = "??:??";
-		timeElem.style.display = "inline-block";
-		headerTimer.appendChild(timeElem);
-		header.parentNode.appendChild(headerTimer);
-		//updateTimer();
+    var timeElem;
+    function createTimer(){
+	var header = document.getElementsByClassName("niantic-wayfarer-logo")[0];
+	var headerTimer = document.createElement("div");
+	headerTimer.innerText = "Time: ";
+	headerTimer.setAttribute("style", "display: inline-block; margin-left: 5em;");
+	timeElem = document.createElement("div");
+	timeElem.innerText = "??:??";
+	timeElem.style.display = "inline-block";
+	headerTimer.appendChild(timeElem);
+	header.parentNode.appendChild(headerTimer);
+	updateTimer();
+    }
+
+    function updateTimer(){
+	var tDiff = nSubCtrl.pageData.expires - Date.now();
+
+	if (tDiff > 0){
+		var tDiffMin = Math.floor(tDiff/1000/60);
+		var tDiffSec = Math.ceil(tDiff/1000 - 60*tDiffMin);
+
+		timeElem.innerText = pad(tDiffMin,2) + ":" + pad(tDiffSec,2);
+		//Retrigger function in 1 second
+		setTimeout(updateTimer, 1000);
+	} else {
+		timeElem.innerText = "EXPIRED!";
+		timeElem.setAttribute("style", "color: red;");
 	}
+    }
+
+    function pad(num, size) {
+	var s = num+"";
+	while (s.length < size) s = "0" + s;
+	return s;
+    }
 	
-	createTimer();
+    createTimer();
 
     var S2 = w.S2 = { L: {} };
 
