8e8caf793bd7d8d130eaea7b17824d8e947c8244
delphy79 <56617064+delphy79@users.noreply.github.com>
2020-04-20 12:10:22 +0900

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -596,7 +596,7 @@ function fn_init() {
 	    var topBtn = document.createElement('button');
 	    topBtn.setAttribute("style", "z-index: 9999; position: absolute; left: 0em; top: 50%;");
 	    topBtn.onclick = function() {
-		document.body.scrollTop = 0;
+		document.html.scrollTop = 0;
 	    };
 	    var topBtnText = document.createTextNode('Top');
 	    topBtn.appendChild(topBtnText);
