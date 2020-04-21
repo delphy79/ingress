5f00e46c25da2076f49073be7ea8af6e90c37d3d
delphy79 <56617064+delphy79@users.noreply.github.com>
2020-04-21 15:36:38 +0900

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -596,7 +596,8 @@ function fn_init() {
 	    var topBtn = document.createElement('button');
 	    topBtn.setAttribute("style", "z-index: 9999; position: absolute; left: 0em; top: 70%; background-color: #000; color: #fff; opacity: 0.5;");
 	    topBtn.onclick = function() {
-		document.getElementById("AnswersController").scrollIntoView(true);
+		//document.getElementById("AnswersController").scrollIntoView(true);
+		document.getElementsByClassName("header").scrollIntoView(true);
 	    };
 	    var topBtnText = document.createTextNode('Top');
 	    topBtn.appendChild(topBtnText);
